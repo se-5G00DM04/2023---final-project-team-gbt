fb743ea5e4c6412773d7b5b685953eed1c4b94ae
Gopinathh22 <22IMC10241@fh-krems.ac.at>
2024-01-07 14:07:38 +0100

    fixed Issue:

--- backend/routes/shoppingRouter.js
+++ backend/routes/shoppingRouter.js
@@ -38,6 +38,7 @@ router.post("/", (req, res) => {
     return res.status(400).json({ error: "Name is a required field." });
   }
   newShopping.id = SHOPPING.length + 1;
+  newShopping.items = newShopping.items || 1;
   SHOPPING.push(newShopping);
   res.status(201).json(newShopping);
 });
